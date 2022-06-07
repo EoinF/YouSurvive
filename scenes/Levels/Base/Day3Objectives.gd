56b93f23c38989324cd84e4a83111171ee651c59
Eoin Flanagan <flanagep@tcd.ie>
2022-06-07 16:13:07 +0100

    Connect up initial objectives for experimenter Day3

--- scenes/Levels/Base/Day3Objectives.gd
+++ scenes/Levels/Base/Day3Objectives.gd
@@ -2,54 +2,52 @@ extends Node
 
 signal objectives_updated(objectives)
 
-var OBJECTIVE_COLLECT_COCONUTS_KEY = "OBJECTIVE_COLLECT_COCONUTS"
 var OBJECTIVE_COLLECT_COCONUTS_TEMPLATE = "Collect coconuts (%d/%d)"
 
-var OBJECTIVE_COLLECT_BRANCHES_KEY = "OBJECTIVE_COLLECT_BRANCH"
 var OBJECTIVE_COLLECT_BRANCHES_TEMPLATE = "Collect branches (%d/%d)"
 
-var is_objective_1_complete = false
-var is_objective_2_complete = false
-
-var current_coconuts = 0
-var required_coconuts = 10
+var coconuts_collected = 0
+var coconuts_required = 10
 
 var branches_collected = 0
 var branches_required = 10
 
+var is_objectives_updated = false
+var is_collect_items_active = false
+
+func _process(_delta):
+	# We pool together objective updates and emit them all at once
+	if is_objectives_updated:
+		is_objectives_updated = false
+		emit_signal("objectives_updated", _get_objectives())
+
+
 func _ready():
-	emit_signal("objectives_updated", _get_objectives())
+	is_objectives_updated = true
+
+
+func set_objective_active(type, is_active = true):
+	if type == "collect_items":
+		is_collect_items_active = is_active
+	is_objectives_updated = true
 
 
 func _get_objectives():
-	var objective1 = {
+	return [{
 		"description": OBJECTIVE_COLLECT_BRANCHES_TEMPLATE % [branches_collected, branches_required],
-		"key": OBJECTIVE_COLLECT_BRANCHES_KEY,
-		"is_complete": is_objective_1_complete,
-		"is_visible": true
-	}
-	var objective2 = {
-		"description": OBJECTIVE_COLLECT_COCONUTS_TEMPLATE % [current_coconuts, required_coconuts],
-		"key": OBJECTIVE_COLLECT_COCONUTS_KEY,
-		"is_complete": is_objective_2_complete,
-		"is_visible": true
-	}
-	return [objective1, objective2]
+		"is_complete": branches_collected >= branches_required,
+		"is_visible": is_collect_items_active
+	}, {
+		"description": OBJECTIVE_COLLECT_COCONUTS_TEMPLATE % [coconuts_collected, coconuts_required],
+		"is_complete": coconuts_collected >= coconuts_required,
+		"is_visible": is_collect_items_active
+	}]
 	
 
 func _on_Islander_inventory_slot_change(inventory_slot):
 	if inventory_slot.item_type == "branch":
 		branches_collected = inventory_slot.amount
-
-		if branches_collected == branches_required:
-			is_objective_1_complete = true
-		
-
+		is_objectives_updated = true
 	if inventory_slot.item_type == "coconut":
-		current_coconuts = inventory_slot.amount
-
-		if current_coconuts == required_coconuts:
-			is_objective_2_complete = true
-
-	if inventory_slot.item_type == "coconut" or inventory_slot.item_type == "branch":
-		emit_signal("objectives_updated", _get_objectives())
+		coconuts_collected = inventory_slot.amount
+		is_objectives_updated = true
--- scenes/Levels/Experimenter/Day3/Events.gd
+++ scenes/Levels/Experimenter/Day3/Events.gd
@@ -6,5 +6,13 @@ func _on_DialogueManager_trigger_event(event_name):
 	call(event_name)
 
 
+func enable_controls():
+	get_owner().get_node("Experimenter").enable_controls()
+
+
+func add_collection_goal():
+	get_owner().get_node("Day3Objectives").set_objective_active("collect_items")
+
+
 func gift_crabs():
 	get_owner().get_node("Experimenter").pick_up_item("crab", 25)
