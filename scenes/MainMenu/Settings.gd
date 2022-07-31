9cbb5da2abe941fc9cfe4643d9515b30ba021016
Eoin Flanagan <flanagep@tcd.ie>
2022-07-31 12:44:45 +0100

    Added audio sliders in settings scene
    * Added all sfx to sfx bus
    * Added all music to music bus

--- scenes/MainMenu/Settings.gd
+++ scenes/MainMenu/Settings.gd
@@ -1,3 +1,24 @@
 extends Control
 
 signal finish_scene
+
+
+func _on_MainVolumeSlider_value_changed(value):
+	set_volume(0, value)
+
+
+func _on_MusicVolumeSlider_value_changed(value):
+	set_volume(1, value)
+
+
+func _on_SfxVolumeSlider_value_changed(value):
+	set_volume(2, value)
+
+
+func set_volume(index, pct):
+	AudioServer.set_bus_volume_db(index, linear2db(pct))
+	AudioServer.set_bus_mute(index, pct == 0)
+
+
+func _on_Back_Button_pressed():
+	emit_signal("finish_scene")
