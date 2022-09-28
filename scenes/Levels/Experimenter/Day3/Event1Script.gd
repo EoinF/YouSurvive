20b74d9c1498c326923536441d159dd4ee39e0a9
Eoin Flanagan <flanagep@tcd.ie>
2022-09-28 16:24:47 +0100

    Removed some unused files

--- scenes/Levels/Experimenter/Day3/Event1Script.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-signal finish_event
-
-func trigger():
-	get_owner().get_node("Experimenter").pick_up_item("crab", 50)
-	emit_signal("finish_event")
--- scenes/Levels/Experimenter/Day3/Event2Script.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-signal finish_event
-
-func trigger():
-	var experimenter = get_owner().get_node("Experimenter")
-	experimenter.enable_controls()
-	experimenter.set_is_following(false)
-	emit_signal("finish_event")
